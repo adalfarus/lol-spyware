b943f266d841c41a44935984a0cd6818c8da0210
Adalfarus <51338419+adalfarus@users.noreply.github.com>
2023-04-27 14:59:13 +0200

    Update runner_example.py

--- runner_example.py
+++ runner_example.py
@@ -4,7 +4,7 @@ var = input("Var (0, 1, 2 or 3--0 takes the longest, but is the best, 2 is the f
 ex_num = input("Execute Number of Times (How many Images do you want? e for all): ")
 length = input("Length of URL (Max should be seven, the longer the URL the newer the images): ")
 
-subprocess.run(["python", "LOL_Spyware.py", var, ex_num, length])
+subprocess.run(["python", "LOLSpyware-v1.1.py", var, ex_num, length])
 subprocess.run(["python", "Remove_Samples.py"])
 subprocess.run(["python", "Extract_Key_Data.py"])
 subprocess.run(["python", "Delete_Deuplicate_Files.py"])
